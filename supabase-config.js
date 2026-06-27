6a6797b228076784b66418b504f061b24cbbe9ea
ABEL-GTR <abelgeorgevs@gmail.com>
2026-06-27 10:44:20 +0530

    Update Supabase credentials to use SiliconIDE project instead of AquaFlow

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 // SiliconIDE - Supabase Credentials Configuration
 window.SUPABASE_CONFIG = {
-    url: "https://akemtrmzgfauirpbmqzb.supabase.co",
-    anonKey: "sb_publishable_M66sSvwbSJR7Wx19AW6lvQ_RbTKHFe-"
+    url: "https://yzdrzwqjlgjfmulpbhbf.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6ZHJ6d3FqbGdqZm11bHBiaGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODk4MzQsImV4cCI6MjA5ODA2NTgzNH0.w2WR-HCOj6osJLnhQlY1eZla5aauz94Ko2H6PcuYppU"
 };
+
